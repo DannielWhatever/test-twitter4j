271d6f4068e0a6bb7f3994c7c9f46532fe3aa976
Daniel <dannielgutierrez90@gmail.com>
2016-02-29 16:41:26 -0300

    added

--- src/main/java/cl/app/App.java
+++ src/main/java/cl/app/App.java
@@ -1,11 +1,15 @@
 package cl.app;
 
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.streaming.Durations;
+import org.apache.spark.streaming.api.java.JavaStreamingContext;
+import twitter4j.*;
 import twitter4j.conf.ConfigurationBuilder;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -22,19 +26,39 @@ public class App {
                 .setOAuthAccessTokenSecret("xloT86ISqVW5koFdiYFEhUPqxmzcOtvaRJWKd4SII5A4F");
         TwitterFactory tf = new TwitterFactory(cb.build());
         Twitter twitter = tf.getInstance();
-        try {
-            List<Status> statuses;
-            String screenname = "cht_informatica";
-            statuses = twitter.getUserTimeline(screenname);
-            System.out.println("Showing @" + screenname + "'s user timeline.");
-            for (Status status : statuses) {
-                System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText());
+        List<Status> statuses = new ArrayList<Status>();
+        String screenname = "cht_informatica";
+
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("asdads");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        try{
+            for(int i=1;i<=5;i++) {
+                int oldSize = statuses.size();
+                statuses.addAll(twitter.getUserTimeline(screenname, new Paging(i, 200)));
+
+                if(statuses.size()==oldSize)
+                    break;
             }
-        } catch (TwitterException te) {
-            te.printStackTrace();
-            System.out.println("Failed to get timeline: " + te.getMessage());
-            System.exit(-1);
+        }catch(TwitterException e){
+            e.printStackTrace();
         }
+        JavaRDD<Status> rdd = sc.parallelize(statuses);
+        System.out.println("Showing @" + screenname + "'s user timeline.");
+        rdd.foreach(
+                (VoidFunction<Status>) status ->
+                        System.out.println("@" + status.getUser().getScreenName() + " - " + status.getText())
+        );
+        System.out.println("count: "+statuses.size());
+
+        // Create a local StreamingContext with two working thread and batch interval of 1 second
+//        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("NetworkWordCount");
+        //JavaStreamingContext jssc = new JavaStreamingContext(conf, Durations.seconds(1));
+
+        // Create a DStream that will connect to hostname:port, like localhost:9999
+        //JavaReceiverInputDStream<String> lines = jssc.socketTextStream("localhost", 9999);
+        //jssc.queueStream()
+
     }
 
 }
