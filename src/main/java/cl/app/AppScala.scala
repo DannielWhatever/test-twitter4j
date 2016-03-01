f2095ca528c6d728289a4e19d36e33ff58d9cd18
Daniel <dannielgutierrez90@gmail.com>
2016-03-01 14:52:34 -0300

    rango de horas

--- src/main/java/cl/app/AppScala.scala
+++ src/main/java/cl/app/AppScala.scala
@@ -3,7 +3,12 @@ package cl.app
 object AppScala {
 
   def main(args: Array[String]): Unit = {
+
     println("Hello, world!")
+
+    val statuses = TwitterCli.getTweets("cht_informatica")
+    statuses.foreach{println}
+
   }
 
 }
--- src/main/java/cl/app/TwitterCli.scala
+++ src/main/java/cl/app/TwitterCli.scala
@@ -1,9 +1,46 @@
 package cl.app
 
+import java.util
+
+import twitter4j._
+import twitter4j.conf.ConfigurationBuilder
+
+import scala.collection.JavaConverters._
+
 /**
  *
  * @author daniel.gutierrez
  */
 object TwitterCli {
+
+
+  private var twitter: Twitter = null
+
+  val cb = new ConfigurationBuilder()
+  cb.setDebugEnabled(true)
+    .setOAuthConsumerKey("DsWFDg7QFvSKvQyjDEIuvbfL5")
+    .setOAuthConsumerSecret("kR8B4k0EBFrnFWzflFHalw57sPetAAjLqyVfpvq36gPGtI5XOk")
+    .setOAuthAccessToken("2755789148-QQQfRKwzA1fYBW3RQlCvPfnZlROHE7CTX8nHTLC")
+    .setOAuthAccessTokenSecret("xloT86ISqVW5koFdiYFEhUPqxmzcOtvaRJWKd4SII5A4F")
+  twitter = new TwitterFactory(cb.build()).getInstance()
+
+
+  def getTweets(screenname: String): List[Status] = {
+    val statuses = new util.ArrayList[Status]
+    try {
+      var i = 1
+      var done = false
+      while (i <= 5 && !done) {
+        val oldSize = statuses.size
+        statuses.addAll(twitter.getUserTimeline(screenname, new Paging(i, 200)))
+        if (statuses.size == oldSize)
+          done = true
+        i += 1
+      }
+    } catch {
+      case e: TwitterException => e.printStackTrace
+    }
+    return statuses.asScala.toList
+  }
 
 }
